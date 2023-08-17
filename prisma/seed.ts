66b3c87e6e43f240331638d48ecd3ef6a82cd9b2
Jacob Do <jekabs.donis@gmail.com>
2023-08-17 21:48:32 +0200

    initial commit

---
+++ prisma/seed.ts
@@ -0,0 +1,62 @@
+import { PrismaClient } from "@prisma/client";
+import { addDays, setHours, startOfHour } from "date-fns";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const crownSeaways = await prisma.vessel.create({
+    data: {
+      name: "Crown Seaways",
+    },
+  });
+
+  const pearlSeaways = await prisma.vessel.create({
+    data: {
+      name: "Pearl Seaways",
+    },
+  });
+
+  // Seeding voyages
+  for (let i = 0; i < 10; i++) {
+    const departingFromCopenhagenVessel =
+      i % 2 === 0 ? pearlSeaways.id : crownSeaways.id;
+    const departingFromOsloVessel =
+      i % 2 === 0 ? crownSeaways.id : pearlSeaways.id;
+
+    const scheduledDeparture = startOfHour(
+      setHours(addDays(new Date(), i), 15)
+    );
+    const scheduledArrival = startOfHour(
+      setHours(addDays(new Date(), i + 1), 9)
+    );
+
+    await prisma.voyage.create({
+      data: {
+        portOfLoading: "Copenhagen",
+        portOfDischarge: "Oslo",
+        vesselId: departingFromCopenhagenVessel,
+        scheduledDeparture,
+        scheduledArrival,
+      },
+    });
+
+    await prisma.voyage.create({
+      data: {
+        portOfLoading: "Oslo",
+        portOfDischarge: "Copenhagen",
+        vesselId: departingFromOsloVessel,
+        scheduledDeparture,
+        scheduledArrival,
+      },
+    });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
