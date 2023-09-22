d0d502a3eaeeaab31710192482c1338f7187eb4c
Harshit Ravi <harshit.ravi@maersk.com>
2023-09-22 17:50:35 +0200

    Create Api with static data

--- src/pages/api/voyage/createVoyage.ts
+++ src/pages/api/voyage/createVoyage.ts
@@ -1,38 +1,30 @@
-// import type { Vessel, Voyage } from "@prisma/client";
-// import type { NextApiHandler, NextApiResponse, NextApiRequest } from "next";
-// import { prisma } from "~/server/db";
+import type { Vessel, Voyage } from "@prisma/client";
+import type { NextApiHandler, NextApiResponse, NextApiRequest } from "next";
+import { prisma } from "~/server/db";
+import { addDays, setHours, startOfHour } from "date-fns";
 
-// export type ReturnType = (Voyage & { vessel: Vessel })[];
+export type ReturnType = (Voyage & { vessel: Vessel })[];
 
-// const handler: NextApiHandler = async (
-//   req: NextApiRequest,
-//   res: NextApiResponse<undefined>
-// ) => {
-//   if (req.method === "POST") {
-//     const createVoyage = await prisma.voyage.create({
-//       data: {
-//         id: "clmueiyzt0005oi78vt4viakk00",
-//         portOfLoading: "Oslo",
-//         portOfDischarge: "Copenhagen",
-//         vesselId: "clmueiyyv0000oi78snlmkcbv",
-//         scheduledDeparture: "2023-09-22T13:00:00.000Z",
-//         scheduledArrival: "2023-09-23T07:00:00.000Z",
-//         createdAt: "2023-09-22T09:30:42.473Z",
-//         updatedAt: "2023-09-22T09:30:42.473Z",
-//         vessel: {
-//           name: "Crown Seaways",
-//           createdAt: "2023-09-22T09:30:42.440Z",
-//           updatedAt: "2023-09-22T09:30:42.440Z",
-//         },
-//       },
-//     });
+const handler: NextApiHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse<undefined>
+) => {
+  if (req.method === "POST") {
+    const createVoyage = await prisma.voyage.create({
+      data: {
+        portOfLoading: "Copenhagen",
+        portOfDischarge: "Oslo",
+        vesselId: "clmueiyyv0000oi78snlmkcbv",
+        scheduledDeparture: startOfHour(setHours(addDays(new Date(), 1), 15)),
+        scheduledArrival: startOfHour(setHours(addDays(new Date(), 2), 15)),
+      },
+    });
+    createVoyage ? res.status(202) : res.status(404);
+    res.end();
+    return;
+  }
 
-//     createVoyage ? res.status(204) : res.status(404);
-//     res.end();
-//     return;
-//   }
+  res.status(405).end();
+};
 
-//   res.status(405).end();
-// };
-
-// export default handler;
+export default handler;
